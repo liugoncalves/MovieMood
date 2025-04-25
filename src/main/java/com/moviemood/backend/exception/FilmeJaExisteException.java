aa810637ed0e744493b8906b26bdd1f118570e7a
liugoncalves <110125502+liugoncalves@users.noreply.github.com>
2025-04-25 11:16:31 -0300

    chore: remove arquivos do controle de versão que estavam no .gitignore

--- src/main/java/com/moviemood/backend/exception/FilmeJaExisteException.java
+++
@@ -1,7 +0,0 @@
-package com.moviemood.backend.exception;
-
-public class FilmeJaExisteException extends RuntimeException {
-    public FilmeJaExisteException(String message) {
-        super(message);
-    }
-}
